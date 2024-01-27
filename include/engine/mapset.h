23f724b90f3e564d9d492cda7cd04f655c04244c
Mallow <mallory.scotton@epitech.eu>
2024-01-28 00:05:14 +0100

    [ADD] Adding tiles parsing.

--- include/engine/mapset.h
+++ include/engine/mapset.h
@@ -14,6 +14,197 @@
     #include "engine/dependencies.h"
     #include "engine/types.h"
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Structure for containing the tiles used in the assets
+///
+/// \param index            The index of the sprite, for level
+/// \param zone_name        The zone name, to load the assets or not
+/// \param file_path        The local/absolute assets path
+/// \param browser_path     Used for the level editor
+/// \param animated         Boolean, is the tiles animated or a tilemap
+/// \param preview          Did the animation will play in the editor
+/// \param texture          The texture object, when the file is loaded
+/// \param mask             The mask used for the animation
+/// \param size             The complete size of the image
+/// \param grid             The animation grid size, without animation (1,1)
+/// \param frame            The size of one frame of the grid
+/// \param endframe         The index of the last frame of the animation
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    ulong index;
+    string zone_name;
+    string file_path;
+    string browser_path;
+    string class_name;
+    bool animated;
+    bool preview;
+    sfTexture *texture;
+    sfIntRect *mask;
+    vec2u size;
+    vec2u grid;
+    vec2u frame;
+    uint endframe;
+} tile_t;
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Structure to hold the audio used in the game
+///
+/// \param file_path        The local/absolute path of the audio file
+/// \param browser_path     If NULL, we won't show the audio in the editor
+/// \param looped           Boolean, set to repeat or not the sound
+/// \param canal            Canal id to output the audio
+/// \param volume           Relative volume of the sound [0-100]
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    string file_path;
+    string browser_path;
+    bool looped;
+    uint canal;
+    uint volume;
+} audio_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Structure to hold the animation of a sprite
+///
+/// \param name             The animation name, used to play the anim
+/// \param start            The index of the first frame of the animation
+/// \param end              The index of the last frame of the animation
+/// \param looped           Boolean to set the animation repeatly
+/// \param framerate        If set to DEFAULT, or can be custom, represent
+///                         the animation framerate
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    string name;
+    uint start;
+    uint end;
+    bool looped;
+    uint framerate;
+} animation_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Structure to hold repeated animation
+///
+/// \param name         The name of the animation sheet
+/// \param animations   The animations array
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    string name;
+    animation_t *animations;
+} animation_sheet_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Structure to hold a sprite object
+///
+/// \param file_path        The local/absolute path of the image
+/// \param animated         Boolean if the sprite is using animations
+/// \param texture          The texture object to avoid loading the assets
+///                         in runtime
+/// \param mask             The texture mask for animation
+/// \param size             The complete size of the image
+/// \param grid             The grid size, represent the number of frame in the
+///                         x and y axis
+/// \param frame            The frame size, all frame should have the same
+///                         size
+/// \param animations       Array of animations, use to switch between the
+///                         animations.
+/// \param animation_count  Keep an eye on the number of animation
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    string file_path;
+    bool animated;
+    sfTexture *texture;
+    sfIntRect *mask;
+    vec2u size;
+    vec2u grid;
+    vec2u frame;
+    animation_t *animations;
+    ulong animation_count;
+} sprite_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Mapset structure to hold every loaded/referenced assets
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    tile_t **tiles;
+    ulong tile_count;
+    audio_t **audios;
+    ulong audio_count;
+    sprite_t **sprites;
+    ulong sprite_count;
+    animation_sheet_t **animations;
+    uint animation_count;
+    float version;
+    uint default_framerate;
+    bool use_browser;
+    bool in_zone;
+    string zone_name;
+} mapset_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Assets action to hold the mapset and every possible action
+/// on the mapset
+///
+/// \param mapset       The global mapset to load/unload assets
+/// \param init         Init the unzoned assets and allocate the memory
+/// \param load         Load a specific zone of assets
+/// \param unload       Unload a specific zone of the assets
+/// \param destroy      Unload every assets and clear the mapset
+///
+///////////////////////////////////////////////////////////////////////////////
+typedef struct {
+    mapset_t *mapset;
+    bool (*init)(void);
+    bool (*load)(cstring zone);
+    bool (*unload)(cstring zone);
+    bool (*destroy)(void);
+} assets_action_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+///////////////////////////////////////////////////////////////////////////////
+extern assets_action_t Assets;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief
+///
+///////////////////////////////////////////////////////////////////////////////
+bool mapset_load(string source);
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Initialize the assets structures
+///
+///////////////////////////////////////////////////////////////////////////////
+bool init_assets(void);
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Parse and apply configuration settings from a mapset file line
+///
+/// \param l  The input line from the mapset configuration file
+///
+///////////////////////////////////////////////////////////////////////////////
+void mapset_parse_config(string l);
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Parse and handle zone definitions from a mapset file line
+///
+/// \param l  The input line from the mapset file defining a zone
+///
+///////////////////////////////////////////////////////////////////////////////
+void mapset_parse_zone(string l);
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Parse and handle mapset tile definitions from a line
+///
+/// \param l  The input line from the mapset file defining a tile
+///
+///////////////////////////////////////////////////////////////////////////////
+void mapset_parse_tile(string l);
 
 #endif /* !MAPSET_H_ */
---
+++ source/ressources/tiles.c
@@ -0,0 +1,115 @@
+/*
+** EPITECH PROJECT, 2024
+** ak-xolotl
+** File description:
+** tiles
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+// Headers
+///////////////////////////////////////////////////////////////////////////////
+#include "engine.h"
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Validate a mapset tile definition
+///
+///
+/// \param wa  The string array representing a mapset tile definition
+/// \param wl  The length of the string array 'wa'
+///
+/// \return    Returns true if the mapset tile definition is valid, otherwise
+///            false
+///
+///////////////////////////////////////////////////////////////////////////////
+static bool is_mapset_tile_valid(string *wa, ulong wl)
+{
+    RETURN(wl != 4 && wl != 8 && wl != 9 && wl != 5, false);
+    RETURN(!my_isbool(wa[3]), false);
+    if (wl == 8 || wl == 9) {
+        RETURN(my_atob(wa[3]) == false, false);
+        RETURN(!my_isint(wa[4]) || !my_isint(wa[5]), false);
+        RETURN(!my_isint(wa[6]) || !my_isbool(wa[7]), false);
+    }
+    if (wl == 4 || wl == 5)
+        RETURN(my_atob(wa[3]) == true, false);
+    return (true);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Parse animation details for a mapset tile
+///
+/// \param wa  The string array representing a mapset tile definition
+/// \param wl  The length of the string array 'wa'
+/// \param t   Pointer to the tile structure to be updated
+///
+///////////////////////////////////////////////////////////////////////////////
+static void mapset_tile_parse_animation(string *wa, ulong wl, tile_t *t)
+{
+    t->grid.x = (uint)my_atoi(wa[4]);
+    t->grid.y = (uint)my_atoi(wa[5]);
+    t->endframe = (uint)my_atoi(wa[6]);
+    t->preview = my_atob(wa[7]);
+    if (wl == 9)
+        t->class_name = my_strdup(wa[8]);
+    if (Assets.mapset->in_zone == false) {
+        t->frame.x = t->size.x / t->grid.x;
+        t->frame.y = t->size.y / t->grid.y;
+        t->mask->height = t->frame.y;
+        t->mask->width = t->frame.x;
+        t->mask->left = 0;
+        t->mask->top = 0;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Create a mapset tile from a string array
+///
+/// \param wa  The string array representing a mapset tile definition
+/// \param wl  The length of the string array 'wa'
+///
+/// \return    Returns a pointer to the created tile, or NULL on allocation
+///            failure
+///
+///////////////////////////////////////////////////////////////////////////////
+static tile_t *mapset_tile_create(string *wa, ulong wl)
+{
+    tile_t *t = malloc(sizeof(tile_t));
+
+    RETURN(t == NULL, NULL);
+    t->index = Assets.mapset->tile_count;
+    t->file_path = my_strdup(wa[1]);
+    t->browser_path = my_strdup(wa[2]);
+    t->animated = my_atob(wa[3]);
+    t->zone_name = NULL;
+    if (Assets.mapset->in_zone == true) {
+        t->zone_name = my_strdup(Assets.mapset->zone_name);
+    } else {
+        t->texture = sfTexture_createFromFile(t->file_path, NULL);
+        t->size = sfTexture_getSize(t->texture);
+    }
+    if (t->animated == true)
+        mapset_tile_parse_animation(wa, wl, t);
+    else if (wl == 5)
+        t->class_name = my_strdup(wa[4]);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Parse and handle mapset tile definitions from a line
+///
+/// \param l  The input line from the mapset file defining a tile
+///
+///////////////////////////////////////////////////////////////////////////////
+void mapset_parse_tile(string l)
+{
+    string *wa = my_stowa(l);
+    ulong wl = wa ? my_walen(wa) : 0;
+
+    RETURN(wa == NULL, (void)0);
+    RETURN(!is_mapset_tile_valid(wa, wl), my_watroy(wa));
+    my_realloc(Assets.mapset->tiles, sizeof(tile_t) *
+        (Assets.mapset->tile_count + 2));
+    Assets.mapset->tiles[Assets.mapset->tile_count] =
+        mapset_tile_create(wa, wl);
+    Assets.mapset->tile_count++;
+    my_watroy(wa);
+}
