4b2487b5cca296217a366ff00cc6f41a6a66e3e5
Mallow <mallory.scotton@epitech.eu>
2024-01-27 23:32:33 +0100

    [ADD] Adding new macros

--- include/engine/macros.h
+++ include/engine/macros.h
@@ -11,12 +11,18 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// \brief Macro to check if a character is a digit
 ///
-/// This macro checks whether the given character 'c' is a digit (0-9).
-///
 /// \param c     The character to check
 ///
 /// \return      Returns true if 'c' is a digit, otherwise false
 ///////////////////////////////////////////////////////////////////////////////
-#define S_DIGIT(c) ((c) >= '0' && (c) <= '9')
+    #define S_DIGIT(c) ((c) >= '0' && (c) <= '9')
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Macro for conditional early return
+///
+/// \param condition  The condition to check
+/// \param value      The value to return if the condition is true
+///////////////////////////////////////////////////////////////////////////////
+    #define RETURN(condition, value) if (condition) return (value)
 
 #endif /* !MACROS_H_ */
