f5ea9ac62971d5948912ecc7ec5c8ac607fc4b16
Mallow <mallory.scotton@epitech.eu>
2024-01-31 22:40:06 +0100

    [ADD] Adding level structures

--- include/engine/level.h
+++ include/engine/level.h
@@ -22,11 +22,80 @@
     #define B_ASSET 11
     #define B_POSITION 8
     #define B_ROTATION 9
-    #define B_SCALE 4
     #define B_BACKGROUND 1
     #define BITTOBYTE(value) ((int)((value) / 8 + 1))
 
 ///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// Level Config
+
+    #define CHUNK_GRID_X 8
+    #define CHUNK_GRID_Y 8
+    #define CHUNK_MAX (CHUNK_GRID_X * CHUNK_GRID_Y)
+    #define MAX_TILE_IN_CHUNK 2048
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Structure representing a tile in a level
+typedef struct {
+    uint tileId;
+    vec2u position;
+    uint rotation;
+} level_tile_t;
+
+// Enumeration representing the type of objects in the level
+typedef enum {
+    object_spawner,
+    object_spawnpoint,
+    object_trap,
+    object_door,
+    object_secret
+} object_type_e;
+
+// Structure representing an object in the level
+typedef struct {
+    object_type_e type;
+    vec2u position;
+} level_object_t;
+
+// Structure representing a chunk of tiles and objects in the level
+typedef struct {
+    bool visible;
+    level_tile_t *tiles[MAX_TILE_IN_CHUNK];
+    uint tile_count;
+    level_object_t **objects;
+    uint object_count;
+} level_chunk_t;
+
+// Enumeration representing the type of areas in the level
+typedef enum {
+    area_hitbox,
+    area_viewbox
+} area_type_e;
+
+// Structure representing an area in the level
+typedef struct {
+    area_type_e area_type;
+    vec2u size;
+    vec2u position;
+} level_area_t;
+
+// Enumeration representing different biomes in the level
+typedef enum {
+    biome_plain,
+    biome_junkyard,
+    biome_asian,
+    biome_aquatic,
+    biome_octopus
+} level_biome_e;
+
+// Structure representing the overall level
+typedef struct {
+    level_biome_e biome;
+    level_chunk_t *chunks[CHUNK_MAX];
+    level_area_t **areas;
+} level_t;
 
 ///////////////////////////////////////////////////////////////////////////////
 /// \brief Read a specified number of bits from a buffer
