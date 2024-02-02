1ccab051f6e94f32ed2c124b4a85c700909292e9
Mallow <mallory.scotton@epitech.eu>
2024-02-02 23:47:48 +0100

    [UPDATE] Update isbool function to handle parenthesis

--- source/shared/numbers/isbool.c
+++ source/shared/numbers/isbool.c
@@ -23,12 +23,15 @@ bool my_isbool(string str)
 {
     int len = my_strlen(str);
 
-    if (len == 1) {
+    if (len == 1 || (len == 2 && str[1] == ')')) {
         RETURN(str[0] == '0' || str[0] == '1', true);
         return (false);
     }
     if (my_strcmp(str, "FALSE") == 0 || my_strcmp(str, "TRUE") ||
         my_strcmp(str, "true") || my_strcmp(str, "false"))
+        return (true);
+    if (my_strcmp(str, "FALSE)") == 0 || my_strcmp(str, "TRUE)") ||
+        my_strcmp(str, "true)") || my_strcmp(str, "false)"))
         return (true);
     return (false);
 }
